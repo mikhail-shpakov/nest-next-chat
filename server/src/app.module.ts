9fb96e4bcea78379a7caefd976040199835e7575
Mikhail Shpakov <mikhail.n.shpakov@gmail.com>
2021-09-19 20:45:09 +0300

    build: исправлены переменные окружения для деплоя

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,11 @@ import { AuthModule } from './auth/auth.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsersModule } from './users/users.module'
 import { MessagesModule } from './messages/messages.module'
-import { SessionsModule } from './sessions/sessions.module';
+import { SessionsModule } from './sessions/sessions.module'
 import * as process from 'process'
 
 const username = process.env.POSTGRES_USER || 'postgres'
+const database = process.env.POSTGRES_DB || username
 const password = process.env.POSTGRES_PASSWORD || 'example'
 const host = process.env.DB_HOST || 'localhost'
 const port = Number(process.env.DB_PORT) || 5432
@@ -19,7 +20,7 @@ const port = Number(process.env.DB_PORT) || 5432
       port,
       username,
       password,
-      database: 'postgres',
+      database,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
